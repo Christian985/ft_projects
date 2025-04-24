d18489712a2fbc39ec11f809fa2f73ff0b69cb0c
chris985 <christianlonardoni@gmail.com>
2025-04-24 13:18:51 -0300

    Initial commit

---
+++ src/list_view.py
@@ -0,0 +1,62 @@
+import flet as ft
+from flet import AppBar, Text, View
+from flet.core.colors import Colors
+
+
+def main(page: ft.Page):
+    # Configurações
+    page.title = "Exemplo de Rotas"
+    page.theme_mode = ft.ThemeMode.DARK  # ou ft.ThemeMode.DARK
+    page.window.width = 375
+    page.window.height = 667
+
+    # Funções
+    def gerencia_rotas(e):
+        page.views.clear()
+        page.views.append(
+            View(
+                "/",
+                [
+                    AppBar(title=Text("Home"), bgcolor=Colors.PRIMARY_CONTAINER),
+                    input_nome,
+                    ft.Button(
+                        text="Salvar",
+                        on_click=lambda _: salvar_nome(e),
+                    )
+                ],
+            )
+        )
+        if page.route == "/segunda":
+            page.views.append(
+                View(
+                    "/segunda",
+                    [
+                        AppBar(title=Text("Segunda tela"), bgcolor=Colors.SECONDARY_CONTAINER),
+                    ],
+                )
+            )
+        page.update()
+
+    def voltar(e):
+        page.views.pop()
+        top_view = page.views[-1]
+        page.go(top_view.route)
+
+
+    # Componentes
+    input_nome = ft.TextField(label="Nome")
+    btn_salvar = ft.Button(text="Salvar")
+
+    lv_nome = ft.ListView(
+        height=500
+    )
+
+    # Eventos
+    page.on_route_change = gerencia_rotas
+    page.on_view_pop = voltar
+
+    page.go(page.route)
+
+# Comando que executa o aplicativo
+# Deve estar sempre colado na linha
+ft.app(main)
