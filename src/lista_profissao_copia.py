e0aa76ea44e3f33cbfeaba5271b2c9e56f3091c1
chris985 <christianlonardoni@gmail.com>
2025-05-09 13:22:13 -0300

    commit

--- src/lista_profissao_copia.py
+++ src/lista_profissao_copia.py
@@ -1,13 +1,18 @@
 import flet as ft
 from flet import AppBar, Text, View
 from flet.core.colors import Colors
-from models import User, db_session
 
+# Classe do Usuário
+class User():
+    def __init__(self, nome, profissao, salario):
+        self.nome = nome
+        self.profissao = profissao
+        self.salario = int(salario)
 
 # Main
 def main(page: ft.Page):
     # Configurações
-    page.title = "Exemplo de Rotas"
+    page.title = "Trabalho"
     page.theme_mode = ft.ThemeMode.DARK  # ou ft.ThemeMode.DARK
     page.window.width = 375
     page.window.height = 667
@@ -16,55 +21,63 @@ def main(page: ft.Page):
     lista = []
 
     # Salva as informações
-    def salvar_nome(e):
-        if input_nome.value == "":
+    def salvar_tudo(e):
+        # Caso eles não possuam valores
+        if input_profissao.value == "" or input_salario.value == "" or input_nome.value == "":
             # Overlay vai apagar a mensagem anterior
             page.overlay.append(msg_erro)
             # Vai abrir a mensagem
             msg_erro.open = True
             page.update()
         else:
-            # Adiciona o valor de input_nome na lista
-            lista.append(input_nome.value)
+            obj_user = User(
+                nome=input_nome.value,
+                profissao=input_profissao.value,
+                salario=input_salario.value,
+            )
+            # Adiciona o Valor de input_nome, input_profissão e input_salário na Lista
+            lista.append(obj_user)
             input_nome.value = ""
+            input_profissao.value = ""
+            input_salario.value = ""
             # Overlay vai apagar a mensagem anterior
             page.overlay.append(msg_sucesso)
             # Vai abrir a mensagem
             msg_sucesso.open = True
             page.update()
-
     # FIM do salvamento
 
-    # Vai exibir a lista
+    # Exibe a Lista
     def exibir_lista(e):
         lv_nome.controls.clear()
-        for nome in lista:
+        for use in lista:
             lv_nome.controls.append(
-                ft.Text(value=nome)
+                ft.Text(value= f'Nome: {use.nome} - Profissão: {use.profissao} - Salário: {use.salario}')
             )
         page.update()
-
     # FIM da exibição da lista
 
-    # Gerencia o caminho das rotas
+    # Gerencia o caminho das Rotas
     def gerencia_rotas(e):
         page.views.clear()
         page.views.append(
-            View(  # Primeira Página
+            View( # Primeira Página
                 "/",
                 [
                     AppBar(title=Text("Home"), bgcolor=Colors.PRIMARY_CONTAINER),
                     input_nome,
-                    # Irá salvar os Nomes
+                    input_profissao,
+                    input_salario,
+                    # Irá salvar os Dados
                     ft.Button(
                         text="Salvar",
-                        on_click=lambda _: salvar_nome(e),
+                        on_click=lambda _: salvar_tudo(e),
                     ),
-                    # Irá mostrar os Nomes
-                    ft.Button(
-                        text="Exibir lista",
-                        on_click=lambda _: page.go("/segunda"),
-                    )
+                        # Irá mostrar os Dados
+                        ft.Button(
+                            text="Exibir lista",
+                            on_click=lambda _: page.go("/segunda"),
+                        )
                 ],
             )
         )
@@ -75,22 +88,18 @@ def main(page: ft.Page):
                 View(
                     "/segunda",
                     [
-                        AppBar(title=Text("Segunda Tela"), bgcolor=Colors.SECONDARY_CONTAINER),
+                        AppBar(title=Text("Lista"), bgcolor=Colors.SECONDARY_CONTAINER),
                         lv_nome,
-                        ft.FloatingActionButton(text="+"),
                     ],
                 )
             )
         page.update()
-
     # FIM da Transição de Páginas
 
-    # Configura a seta para voltar
     def voltar(e):
         page.views.pop()
         top_view = page.views[-1]
         page.go(top_view.route)
-
     # FIM da seta de Voltar
 
     # Componentes
@@ -103,6 +112,8 @@ def main(page: ft.Page):
         bgcolor=Colors.RED
     )
     input_nome = ft.TextField(label="Nome")
+    input_profissao = ft.TextField(label="Profissão")
+    input_salario = ft.TextField(label="Salário")
 
     lv_nome = ft.ListView(
         height=500
@@ -116,7 +127,6 @@ def main(page: ft.Page):
     page.go(page.route)
     # FIM dos Eventos
 
-
-# Comando que executa o aplicativo
+# Comando que executa o Aplicativo
 # Deve estar sempre colado na linha
 ft.app(main)
